36f64be94ce61dd3a540993655d362214d408c00
Thiago Calura <thiagocalura@outlook.com>
2018-11-01 14:27:47 -0300

    gerado contoller sessions e password para user e suas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_scope :user do
+    root to: "users/sessions#new"
+  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
