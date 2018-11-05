3a03576d48781e88d338d746bdec08d8dc150871
Thiago Calura <thiagocalura@outlook.com>
2018-11-05 13:45:14 -0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "João da Silva", email: "a@a.com", password: "123456", password_confirmation: "123456")
+User.create(name: "João da Silva", email: "joao@email.com", password: "123456", password_confirmation: "123456")
 
 
 30.times do 
